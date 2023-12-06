a844daf8a575f467e93deb844aabe808978b98b8
Douceur <contact@ether-source.fr>
2023-12-06 12:56:08 +0100

    add acl package for bootstrap

--- galaxy_install.sh
+++ galaxy_install.sh
@@ -1,3 +1,3 @@
 ansible-galaxy collection install community.docker -p collection/
 ansible-galaxy collection install community.general -p collection/
-ansible-galaxy collection install tocard.utils -p collection/
+ansible-galaxy collection install tocard.utils -p collection/ -U
